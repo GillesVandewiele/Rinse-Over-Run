0f6c9002f342fd28070e1c7e4034a22780a38d6e
GillesVandewiele <gilles.vandewiele@ugent.be>
2019-03-03 09:38:51 +0100

    cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Solution to get the second rank on the leaderboard of the Rinse over Run competition',
+    author='Fatima Yamaha (Gilles Vandewiele, Thomas Mortier, Kiani Lannoye, Jan Van De Vyver)',
+    license='MIT',
+)
